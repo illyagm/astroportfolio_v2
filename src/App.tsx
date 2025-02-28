8c428829becad9721b4df67e69fc87f9bda4e553
Illya G. <igordus@jaumebalmes.net>
2025-02-28 21:02:01 +0100

    Fix analytics implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import styles from './App.module.scss';
 import { useCounterStore } from '@store/counter';
-import Button from '@components/ui/Button';
-import Card from '@components/ui/Card';
-import Layout from '@components/layout/Layout';
 import AppRouter from 'routes/Router';
+import { Analytics } from '@vercel/analytics/react';
 
 const App: React.FC = () => {
   const { count, increment } = useCounterStore();
 
-  return <AppRouter />;
+  return (
+    <>
+      <Analytics />
+      <AppRouter />
+    </>
+  );
 };
 
 export default App;
